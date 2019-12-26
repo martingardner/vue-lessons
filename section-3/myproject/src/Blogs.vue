fcdd9676c3ad5cb5efe67b45590a7c05963c629f
martin gardner II <martingardnerii@gmail.com>
2019-12-26 09:24:09 -0600

    added in axios and axios call to created life cycle

--- section-3/myproject/src/Blogs.vue
+++ section-3/myproject/src/Blogs.vue
@@ -2,15 +2,22 @@
   <div id="blogs">
     <h2>{{ blogTitle }}</h2>
     <button @click="changeTitle">Change Title</button>
+    <div v-for="post in posts" :key="post.id">
+      <h3>{{ post.title }}</h3>
+      <p>{{ post.body }}</p>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "Blogs",
   data() {
     return {
-      blogTitle: "Blogs"
+      blogTitle: "Blogs",
+      posts: []
     };
   },
   methods: {
@@ -23,6 +30,10 @@ export default {
   },
   created() {
     console.log("created hook");
+    axios.get("https://jsonplaceholder.typicode.com/todos/").then(response => {
+      console.log("resp", response);
+      this.posts = response.data;
+    });
   },
   beforeUpdate() {
     console.log("beforeUpdate Hook");
