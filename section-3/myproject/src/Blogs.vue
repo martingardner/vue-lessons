4f271d9357e180ce20e8610c059a85c88fa3b1f0
martin gardner II <martingardnerii@gmail.com>
2019-12-26 09:45:35 -0600

    Filters and Computed Properties lessons of section 3

--- section-3/myproject/src/Blogs.vue
+++ section-3/myproject/src/Blogs.vue
@@ -1,10 +1,12 @@
 <template>
   <div id="blogs">
     <h2>{{ blogTitle }}</h2>
+    <input type="text" v-model="searchTerm" />
+    <br />
     <button @click="changeTitle">Change Title</button>
-    <div v-for="post in posts" :key="post.id">
+    <div v-for="post in filteredPosts" :key="post.id">
       <h3>{{ post.title }}</h3>
-      <p>{{ post.body }}</p>
+      <p>{{ post.body | snippet }}</p>
     </div>
   </div>
 </template>
@@ -17,7 +19,8 @@ export default {
   data() {
     return {
       blogTitle: "Blogs",
-      posts: []
+      posts: [],
+      searchTerm: ""
     };
   },
   methods: {
@@ -25,15 +28,27 @@ export default {
       this.blogTitle = "Blog Title Change";
     }
   },
+  computed: {
+    filteredPosts() {
+      return this.posts.filter(post => {
+        return post.title.match(this.searchTerm);
+      });
+    }
+  },
   beforeCreate() {
     console.log("beforeCreate hook");
   },
   created() {
     console.log("created hook");
-    axios.get("https://jsonplaceholder.typicode.com/todos/").then(response => {
-      console.log("resp", response);
-      this.posts = response.data;
-    });
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos/")
+      .then(response => {
+        console.log("resp", response);
+        this.posts = response.data;
+      })
+      .catch(err => {
+        console.log("err", err);
+      });
   },
   beforeUpdate() {
     console.log("beforeUpdate Hook");
--- section-3/myproject/src/main.js
+++ section-3/myproject/src/main.js
@@ -1,7 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+
+// create global filter
+Vue.filter("snippet", val => {
+  if (!val || typeof val !== "string") {
+    return "";
+  }
+  val = val.slice(0, 50);
+  return val;
+});
 
 new Vue({
-  el: '#app',
+  el: "#app",
   render: h => h(App)
-})
+});
