0e7c37d7513113b1da1d0ae2f63416c0729d1478
martin gardner II <martingardnerii@gmail.com>
2020-01-02 10:58:25 -0600

    start section-4 vue-router, add in vuerouter project

---
+++ section-4/vuerouter/src/components/Navbar.vue
@@ -0,0 +1,32 @@
+<template>
+  <nav class="main-nav">
+    <ul>
+      <li>
+        <router-link to="/">Home</router-link>
+      </li>
+      <li>
+        <router-link :to="{ name: 'About'}">About</router-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style scoped>
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+a {
+  color: green;
+}
+</style>
