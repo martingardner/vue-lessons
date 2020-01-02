68f9a1f484bca6035f03e380c2023e758925b9ee
martin gardner II <martingardnerii@gmail.com>
2020-01-02 11:52:18 -0600

    viewProfile file and how to send dynamic routes

---
+++ section-4/vuerouter/src/components/ViewProfile.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="view-profile">
+    <h2>Profile for {{ userId }}</h2>
+  </div>
+</template>
+
+<script>
+/*
+ watch and call to updateId exists because otherwise if you navigate from viewProfile directly to viewProfile it doesn't update the component.  The watch 
+ exists to force it to reupdate.
+*/
+export default {
+  name: "ViewProfile",
+  data() {
+    return {
+      userId: this.$route.params.user_id //generated in router/index.js
+    };
+  },
+  methods: {
+    updateId() {
+      this.userId = this.$route.params.user_id;
+    }
+  },
+  watch: {
+    $route: "updateId"
+  }
+};
+</script>
+
+<style scoped>
+</style>
