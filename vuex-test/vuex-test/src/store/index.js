577bab6b2105e27dc219f1cd4cbb0f0426f97f6e
martin gardner II <martingardnerii@gmail.com>
2020-01-10 14:31:28 -0600

    update vuex lesson with state and getters example

--- vuex-test/vuex-test/src/store/index.js
+++ vuex-test/vuex-test/src/store/index.js
@@ -1,15 +1,21 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    title: "Vuex Store",
+    todos: [
+      { id: 1, title: "One" },
+      { id: 2, title: "Two" }
+    ]
   },
-  mutations: {
+  getters: {
+    allTodos: state => state.todos,
+    getTitle: state => state.title
   },
-  actions: {
-  },
-  modules: {
-  }
-})
+  mutations: {},
+  actions: {},
+  modules: {}
+});
